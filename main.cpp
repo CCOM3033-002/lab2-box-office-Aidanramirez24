cadbff25a6564258c2d1f12cae564e52620bba00
Aidanramirez24 <aidan.ramirez@upr.edu>
2024-09-06 19:28:42 +0000

    starting

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <iomanip> // el uso de iomanip es porque para este projecto se usan muchos input manipulators
+using namespace std; // basic starter for programming in c++
 
+
+int main()
+{
+    int adult_tickets, child_tickets;
+    char movie_name;
+    double gross_box, net_box, paid;
+
+    cout << "This program calculates the Gross and Net Box office profit and the distributer's profit of a movie playing in a theater." << endl;
+
+    cout << "please enter the movie name:" << endl;
+
+    
+    cin >> movie_name;
+
+    cout << "Please enter the amount of adult tickets sold:"<< endl;
+
+    cin >> adult_tickets;
+
+    cout << "please enter the amount of child tickets sold:" 
+
+    cin >> child_tickets;
+
+
+
+
+
+}
